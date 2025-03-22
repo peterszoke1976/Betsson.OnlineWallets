40e0067d94e67328c4bb3a4925810eb1b01dc411
peterszoke1976 <peterszoke1976@googlemail.com>
2025-03-22 14:08:44 +0100

    Add the new tests projects to the solution. Add the first unit test.

---
+++ tests/Betsson.OnlineWallets.Data.IntegrationTests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Betsson.OnlineWallets.Data.IntegrationTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ tests/Betsson.OnlineWallets.UnitTests/OnlineWalletServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using Betsson.OnlineWallets.Data.Models;
+using Betsson.OnlineWallets.Data.Repositories;
+using Betsson.OnlineWallets.Exceptions;
+using Betsson.OnlineWallets.Models;
+using Betsson.OnlineWallets.Services;
+using Moq;
+
+namespace Betsson.OnlineWallets.UnitTests
+{
+    [TestClass]
+    public class OnlineWalletServiceTests
+    {
+        private readonly Mock<IOnlineWalletRepository> _mockRepo;
+        private readonly OnlineWalletService _service;
+
+        public OnlineWalletServiceTests()
+        {
+            _mockRepo = new Mock<IOnlineWalletRepository>();
+            _service = new OnlineWalletService(_mockRepo.Object);
+        }
+
+        [TestMethod]
+        public async Task GetBalanceAsync_NoEntries_ReturnsZero()
+        {
+            // Arrange
+            _mockRepo.Setup(r => r.GetLastOnlineWalletEntryAsync()).ReturnsAsync((OnlineWalletEntry?)null);
+
+            // Act
+            var result = await _service.GetBalanceAsync();
+
+            // Assert
+            //result.Amount.Should().Be(0);
+            Assert.AreEqual(0, result.Amount); // MSTest alternative
+        }
+
+        
+
+        
+    }
+}
